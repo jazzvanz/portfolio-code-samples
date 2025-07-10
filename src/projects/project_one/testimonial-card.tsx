fcd376a57a1341b439c0722d80e2b514735f5fd9
Jazz Vangeel <jazzmin.vangeel@meetdomaine.com>
2025-07-10 15:21:05 -0400

    added basic component and plan

---
+++ src/projects/project_one/testimonial-card.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+// <!-- Sarah Dole
+// @sarahdole
+// I've been searching for high-quality abstract images for my design projects, 
+// and I'm thrilled to have found this platform. The variety and depth of creativity are astounding! 
+// -->
+
+const Title = styled.h2`
+  margin-top: 0;
+  text-align: center;
+`;
+
+const TestimonialCard: React.FC = () => {
+    return (
+        <>
+            <Title>test</Title>
+        </>
+    )
+};
+
+export default TestimonialCard;
+
+// TODO
+// - default CSS / Reformat
+// add to app file / start gallery? 
+// html
+// content - set up mock file
+// setup type file
+// setup theme file - look to design system 
+// acheive layout
+// responsive 
+// check other broswers
+// truncate utlity function 
+// add test for truncating 
+// run through instructions 
+// host for review? 
